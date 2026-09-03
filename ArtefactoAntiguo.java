5ad0e8915bcaa00673da206644c95f008ded27ea
marco <maguevara@uniempresarial.edu.co>
2026-09-03 14:03:12 -0500

    Creacion de los constructores 2 y 3

--- ArtefactoAntiguo.java
+++ ArtefactoAntiguo.java
@@ -11,8 +11,13 @@ public class ArtefactoAntiguo {
         this.pesoGramos = pesoGramos;
         this.esOrganico = esOrganico;
     }
+    public ArtefactoAntiguo(String codigoRegistro, double pesoGramos) {
+        this(codigoRegistro, 2026, pesoGramos, false);
+    }
+        public ArtefactoAntiguo(String codigoRegistro) {
+        this(codigoRegistro, 0.0);
+    }
 
-    
 
 
 
