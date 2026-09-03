faa46830be5c18c7223332291d415420f615a3f0
marco <maguevara@uniempresarial.edu.co>
2026-09-03 13:56:41 -0500

    primer cosntructor de la superclase

--- ArtefactoAntiguo.java
+++ ArtefactoAntiguo.java
@@ -5,4 +5,17 @@ public class ArtefactoAntiguo {
     private double pesoGramos;
     private boolean esOrganico;
 
+    public ArtefactoAntiguo(String codigoRegistro, int anoDescubrimiento, double pesoGramos, boolean esOrganico) {
+        this.codigoRegistro = codigoRegistro;
+        this.anoDescubrimiento = anoDescubrimiento;
+        this.pesoGramos = pesoGramos;
+        this.esOrganico = esOrganico;
+    }
+
+    
+
+
+
+
+
 }
