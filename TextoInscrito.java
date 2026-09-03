d2109bfa66f2f7e1553cb2d9010be9fa99a3c411
marco <maguevara@uniempresarial.edu.co>
2026-09-03 16:32:11 -0500

    creacion constructor subclase TextoInscrito

--- TextoInscrito.java
+++ TextoInscrito.java
@@ -6,4 +6,16 @@ public class TextoInscrito extends ArtefactoAntiguo {
     private long caracteresDescifrados;
 
 
+    public TextoInscrito(String codigoRegistro, int anoDescubrimiento, double pesoGramos, boolean esOrganico,
+                         String idiomaOriginal, int cantidadCaracteres, double anchoTablaCm, long caracteresDescifrados) {
+        super(codigoRegistro, anoDescubrimiento, pesoGramos, esOrganico);
+        this.idiomaOriginal = idiomaOriginal;
+        this.cantidadCaracteres = cantidadCaracteres;
+        this.anchoTablaCm = anchoTablaCm;
+        this.caracteresDescifrados = caracteresDescifrados;
+    }
+
+
+
+
 }
