2153a6ede27f7a7e34163dc0855744df4721fa97
marco <maguevara@uniempresarial.edu.co>
2026-09-03 15:54:33 -0500

    creacion constructor subclase HerramientaPrehistorica

--- HerramientaPrehistorica.java
+++ HerramientaPrehistorica.java
@@ -5,4 +5,14 @@ public class HerramientaPrehistorica extends ArtefactoAntiguo {
     private double grosorMm;
     private boolean tieneMango;
 
+    public HerramientaPrehistorica(String codigoRegistro, int anoDescubrimiento, double pesoGramos, boolean esOrganico,
+                                   String materialPrincipal, int filoEstimadoNewton, double grosorMm, boolean tieneMango) {
+        super(codigoRegistro, anoDescubrimiento, pesoGramos, esOrganico);
+        this.materialPrincipal = materialPrincipal;
+        this.filoEstimadoNewton = filoEstimadoNewton;
+        this.grosorMm = grosorMm;
+        this.tieneMango = tieneMango;
+    }
+
+
 }
