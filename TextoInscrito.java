b253ebf2e89731e24b38486b1718f516cc3f3985
marco <maguevara@uniempresarial.edu.co>
2026-09-03 16:29:07 -0500

    creacion atributos subclase TextoInscrito

--- TextoInscrito.java
+++ TextoInscrito.java
@@ -1,3 +1,9 @@
 public class TextoInscrito extends ArtefactoAntiguo {
     
+    private String idiomaOriginal;
+    private int cantidadCaracteres;
+    private double anchoTablaCm;
+    private long caracteresDescifrados;
+
+
 }
