e023a5b4382d192d6f2447c13e6d79439bbc8e49
marco <maguevara@uniempresarial.edu.co>
2026-09-03 15:03:59 -0500

    creacion atributos subclasr Fosil animal

--- FosilAnimal.java
+++ FosilAnimal.java
@@ -1,3 +1,7 @@
 public class FosilAnimal extends ArtefactoAntiguo  {
     
+    private String especieEstimada;
+    private int antiguedadMillonesAnos;
+    private double longitudMetros;
+    private char estadoConservacion;
 }
