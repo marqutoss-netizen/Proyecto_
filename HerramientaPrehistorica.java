ae4d30bef77383bfd4c05fb26692add22dcabff8
marco <maguevara@uniempresarial.edu.co>
2026-09-03 15:50:15 -0500

    creacion atributos subclase HerramientaPrehistorica

--- HerramientaPrehistorica.java
+++ HerramientaPrehistorica.java
@@ -1,3 +1,8 @@
 public class HerramientaPrehistorica extends ArtefactoAntiguo {
     
+    private String materialPrincipal;
+    private int filoEstimadoNewton;
+    private double grosorMm;
+    private boolean tieneMango;
+
 }
