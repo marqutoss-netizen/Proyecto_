0c8b5eef9c6587a50c0e51d32af85ffb626e116a
marco <maguevara@uniempresarial.edu.co>
2026-09-03 15:11:58 -0500

    creacion primer constructor de subclase Fosilanimal y se le agrega el extenbds para heredar

--- FosilAnimal.java
+++ FosilAnimal.java
@@ -1,7 +1,19 @@
 public class FosilAnimal extends ArtefactoAntiguo  {
-    
+
     private String especieEstimada;
     private int antiguedadMillonesAnos;
     private double longitudMetros;
     private char estadoConservacion;
+
+    
+    public FosilAnimal(String codigoRegistro, int anoDescubrimiento, double pesoGramos, boolean esOrganico,
+                       String especieEstimada, int antiguedadMillonesAnos, double longitudMetros, char estadoConservacion) {
+        super(codigoRegistro, anoDescubrimiento, pesoGramos, esOrganico);
+        this.especieEstimada = especieEstimada;
+        this.antiguedadMillonesAnos = antiguedadMillonesAnos;
+        this.longitudMetros = longitudMetros;
+        this.estadoConservacion = estadoConservacion;
+    }
+
+
 }
